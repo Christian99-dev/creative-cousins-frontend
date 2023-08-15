5c9b5e248819ec40f3d3f2a4a75aa379130c60a2
ckell001 <christiankeller1@student.hs-rm.de>
2023-08-16 00:58:54 +0200

    finish endpoints

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,5 +1,10 @@
 export const staticData = {
-    welcome: "mock/welcome.json",
-    aboutUs: "mock/aboutUs.json",
-    team: "mock/team.json",
-}
+  logo: "mock/logo.json",
+  welcome: "mock/welcome.json",
+  aboutUs: "mock/aboutUs.json",
+  team: "mock/team.json",
+  services: "mock/services.json",
+  gallery: "mock/gallery.json",
+  contact: "mock/contact.json",
+  footer: "mock/footer.json"
+};
--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,6 +3,11 @@ import { staticData } from "./endpoints";
 // chose implementation
 const endpoints = staticData;
 
+// fetch
+export const fetchLogo = async () => {
+    return fetch(endpoints.logo);
+}
+
 export const fetchWelcome = async () => {
     return fetch(endpoints.welcome);
 }
@@ -15,4 +20,19 @@ export const fetchTeam = async () => {
   return fetch(endpoints.team);
 };
 
+export const fetchServices = async () => {
+  return fetch(endpoints.services);
+};
+
+export const fetchGallery = async () => {
+  return fetch(endpoints.gallery);
+};
+
+export const fetchContact = async () => {
+  return fetch(endpoints.contact);
+};
+
+export const fetchFooter = async () => {
+  return fetch(endpoints.footer);
+};
 
