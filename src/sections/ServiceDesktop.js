f84c11a7112e8a2544f4039d1b0e0d39aebd1510
ckell001 <christiankeller1@student.hs-rm.de>
2023-08-21 16:36:07 +0200

    move

--- src/sections/ServiceDesktop.js
+++ src/sections/ServiceDesktop.js
@@ -1,19 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
-import { FetchServices } from "../api/fetch";
-import Switcher from "../components/Switcher";
+import { FetchServices, FetchLogo } from "../api/fetch";
 
 const ServiceDesktop = ({ show }) => {
   const { data, loading } = FetchServices();
   return (
     show && (
-      <ServiceDesktopStyle>
+      <section>
         {!loading && <Switcher items={data.services}></Switcher>}
-      </ServiceDesktopStyle>
+      </section>
     )
   );
 };
 
-export default ServiceDesktop;
+const Switcher = ({ items }) => {
+  const [active, setActive] = useState(-1);
+  const { data, loading } = FetchLogo();
 
-const ServiceDesktopStyle = styled.section``;
+  return (
+    <SwitcherStyle>
+      <div className="left">
+        {items.map((item, index) => {
+          return (
+            <button
+              className={active === index ? "active" : ""}
+              onClick={() => setActive(index)}
+            >
+              {item.name}
+            </button>
+          );
+        })}
+      </div>
+      <div className="right">
+        {active !== -1
+          ? items.map((item, index) => {
+              return (
+                <div
+                  className={
+                    "text-container" + (active === index ? " active" : "")
+                  }
+                >
+                  <p className="name">{item.name}</p>
+                  <p className="description">{item.description}</p>
+                  <img
+                    src={data.logo}
+                    alt="Firmenlogo"
+                    className="logo background"
+                  />
+                </div>
+              );
+            })
+          : !loading && (
+              <div className="logo-container">
+                <img src={data.logo} alt="Firmenlogo" className="logo" />
+              </div>
+            )}
+      </div>
+    </SwitcherStyle>
+  );
+};
+
+const SwitcherStyle = styled.div`
+  height: 550px;
+  display: flex;
+  padding: var(--space-5);
+  margin-bottom: 100px;
+
+  .left {
+    width: 33%;
+    display: flex;
+    flex-direction: column;
+    button {
+      width: 100%;
+      height: 100%;
+      background-color: white;
+      font-size: var(--fs-4);
+      font-weight: 100;
+      color: var(--orange);
+      cursor: pointer;
+      font-style: italic;
+
+      &:hover,
+      &.active {
+        background-color: var(--grey);
+      }
+    }
+  }
+
+  .right {
+    width: 66%;
+    height: 100%;
+
+    .logo-container,
+    .text-container {
+      position: relative;
+      height: 100%;
+
+      .logo {
+        width: 50%;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translateX(-50%) translateY(-50%);
+
+        &.background {
+          opacity: 0.1;
+        }
+      }
+    }
+    .text-container {
+      display: none;
+      flex-direction: column;
+      text-align: center;
+      padding: var(--space-5) var(--space-6);
+      justify-content: center;
+
+      .name {
+        font-size: var(--fs-3);
+        color: var(--orange);
+        padding-bottom: var(--space-1);
+      }
+
+      .description {
+        font-size: var(--fs-4);
+      }
+
+      &.active {
+        display: flex;
+      }
+    }
+  }
+`;
+
+export default ServiceDesktop;
